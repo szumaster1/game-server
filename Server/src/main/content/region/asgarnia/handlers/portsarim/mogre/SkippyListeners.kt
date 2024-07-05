82679b2ff5193316c211bd2256bd2c1e210a8d4e
szumaster <szumaster2009@gmail.com>
2024-07-05 22:38:14 +0000

    Mogre activity fixes & improvements

--- Server/src/main/content/region/asgarnia/handlers/portsarim/mogre/SkippyListeners.kt
+++ Server/src/main/content/region/asgarnia/handlers/portsarim/mogre/SkippyListeners.kt
@@ -8,19 +8,24 @@ import core.game.interaction.InteractionListener
 
 class SkippyListeners : InteractionListener {
 
-    val skippy = intArrayOf(2795,2796,2797,2798,2799)
+    private val skippyWrapperIDs = intArrayOf(2795,2796,2797,2798,2799)
 
     override fun defineListeners() {
-        on(skippy, IntType.NPC, "sober-up") { player, node ->
-            if (getVarbit(player, SkippyUtils.VARBIT_SKIPPY_AND_THE_MOGRES_PROGRESS) == 1) {
-                sendPlayerDialogue(player, "I think he's sober enough. And I don't want to use another bucket of water.")
-            } else {
-                player.dialogueInterpreter.open(node.asNpc().id)
-            }
+
+        /*
+         * Sober up interaction.
+         */
+
+        on(skippyWrapperIDs, IntType.NPC, "sober-up") { player, node ->
+            player.dialogueInterpreter.open(node.id)
             return@on true
         }
 
-        onUseWith(IntType.NPC, Items.FORLORN_BOOT_6663, *skippy) { player, used, _ ->
+        /*
+         * Used Forlorn boots on Skippy.
+         */
+
+        onUseWith(IntType.NPC, Items.FORLORN_BOOT_6663, *skippyWrapperIDs) { player, used, _ ->
             if (removeItem(player, used.asItem())) {
                 sendNPCDialogue(player, NPCs.SKIPPY_2796, "Thanks, now I have two right boots!")
             } else {
@@ -29,7 +34,11 @@ class SkippyListeners : InteractionListener {
             return@onUseWith true
         }
 
-        onUseWith(IntType.NPC, Items.BUCKET_OF_WATER_1929, *skippy) { player, used, _ ->
+        /*
+         * Used bucket of water on skippy (after used before).
+         */
+
+        onUseWith(IntType.NPC, Items.BUCKET_OF_WATER_1929, *skippyWrapperIDs) { player, used, _ ->
             if(getVarbit(player, SkippyUtils.VARBIT_SKIPPY_AND_THE_MOGRES_PROGRESS) > 1 && used.id == 1929){
                 sendPlayerDialogue(player, "I think he's sober enough. And I don't want to use another bucket of water.")
             } else {
