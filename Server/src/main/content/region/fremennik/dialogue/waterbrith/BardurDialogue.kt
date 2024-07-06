ac83dd7b75b9de96014c19e9ba252e59277b2637
szumaster <szumaster2009@gmail.com>
2024-07-06 05:22:55 +0200

    Fixed error with init Bardur dialogue

--- Server/src/main/content/region/fremennik/dialogue/waterbrith/BardurDialogue.kt
+++ Server/src/main/content/region/fremennik/dialogue/waterbrith/BardurDialogue.kt
@@ -21,8 +21,6 @@ class BardurDialogue(player: Player? = null): Dialogue(player) {
      * Dagannoth fledgelings.
      */
 
-    private val fremennikName = player!!.getAttribute("fremennikname", "name")
-
     override fun open(vararg args: Any): Boolean {
         npc = args[0] as NPC
         if (!isQuestComplete(player, "Fremennik Trials")) {
@@ -46,16 +44,16 @@ class BardurDialogue(player: Player? = null): Dialogue(player) {
             1 -> npcl(FacialExpression.FRIENDLY, "Only an outerlander would ask such a question! Is it not obvious what I am doing?").also { stage++ }
             2 -> npcl(FacialExpression.FRIENDLY, "I kill the daggermouths for glory and for sport! When I have had my fill, I move on to the daggermouths' lair!").also { stage++ }
             3 -> playerl(FacialExpression.FRIENDLY, "Erm... okay then. I'll leave you to it.").also { stage = END_DIALOGUE }
-            5 -> npcl(FacialExpression.FRIENDLY, "Listen, $fremennikName. I'm sorry about the way I acted while you were exiled.").also { stage++ }
+            5 -> npcl(FacialExpression.FRIENDLY, "Listen, ${getAttribute(player, "fremennikname", "name")}. I'm sorry about the way I acted while you were exiled.").also { stage++ }
             6 -> sendDialogue(player!!, "Bardur gets hostile around outerlanders.").also { stage++ }
             7 -> playerl(FacialExpression.FRIENDLY, "Don't worry about it, I understand! All's well that ends well! How've you been?").also { stage++ }
             8 -> npcl(FacialExpression.FRIENDLY, "I have been here all the long day, slaughtering the daggermouth spawns so that none may pass!").also { stage++ }
             9 -> npcl(FacialExpression.FRIENDLY, "My sword arm grows weary, and the blood of my foes hangs heavy upon my clothing!").also { stage++ }
             10 -> playerl(FacialExpression.FRIENDLY, "Can you tell me anything about the caves down here?").also { stage++ }
-            11 -> npcl(FacialExpression.FRIENDLY, "Aye, that I can $fremennikName! Yonder lies the lair of the daggermouth kings, the three beasts of legend!").also { stage++ }
+            11 -> npcl(FacialExpression.FRIENDLY, "Aye, that I can $${getAttribute(player, "fremennikname", "name")}! Yonder lies the lair of the daggermouth kings, the three beasts of legend!").also { stage++ }
             12 -> npcl(FacialExpression.FRIENDLY, "I train my skills upon its foul brood, to prepare myself for the fight ahead!").also { stage++ }
             13 -> playerl(FacialExpression.FRIENDLY, "Okay, thanks Bardur. Do you have any food with you?").also { stage++ }
-            14 -> npcl(FacialExpression.FRIENDLY, "Ah, you did not come prepared $fremennikName?").also { stage++ }
+            14 -> npcl(FacialExpression.FRIENDLY, "Ah, you did not come prepared $${getAttribute(player, "fremennikname", "name")}?").also { stage++ }
             15 -> npcl(FacialExpression.FRIENDLY, "I have nothing to spare, but my equipment grows weaker under the onslaught of the dagger-mouth spawns!").also { stage++ }
             16 -> npcl(FacialExpression.FRIENDLY, "I will trade you a finely cooked shark if you have a replacement for my helm, my shield or my blade...").also { stage++ }
             17 -> if(!anyInInventory(player, *BardurExchangeListener.FREMENNIK_EQUIPMENT)) {
