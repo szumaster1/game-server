664b803babf40641972b969c43a7c21e86fa39b0
szumaster <166091306+szumaster3@users.noreply.github.com>
2024-07-10 12:09:45 +0200

    Restored the ability to talk with Curator Haig Halen

--- Server/src/main/content/region/misthalin/dialogue/varrock/museum/CuratorHaigHalenDialogue.kt
+++ Server/src/main/content/region/misthalin/dialogue/varrock/museum/CuratorHaigHalenDialogue.kt
@@ -1,22 +1,19 @@
 package content.region.misthalin.dialogue.varrock.museum
 
-import core.api.consts.NPCs
 import content.region.desert.quest.thegolemquest.dialogue.CuratorHaigHalenGolemDialogue
-import content.region.misthalin.dialogue.digsite.CuratorHaigHelenDialogueFile
 import content.region.misthalin.quest.free.shieldofarrav.dialogue.CuratorHaigHalenSOADialogue
 import core.api.*
 import core.api.consts.Items
+import core.api.consts.NPCs
 import core.game.dialogue.Dialogue
 import core.game.dialogue.FacialExpression
 import core.game.dialogue.IfTopic
 import core.game.dialogue.Topic
-import core.game.node.entity.npc.NPC
 import core.game.node.entity.player.Player
 import core.game.node.entity.player.link.diary.DiaryType
 import core.plugin.Initializable
 import core.tools.END_DIALOGUE
 import core.tools.START_DIALOGUE
-
 
 @Initializable
 class CuratorHaigHalenDialogue(player: Player? = null) : Dialogue(player) {
@@ -26,27 +23,6 @@ class CuratorHaigHalenDialogue(player: Player? = null) : Dialogue(player) {
      *  He can be found on the ground floor.
      *  Location: 3257,3447
      */
-
-    override fun open(vararg args: Any): Boolean {
-        npc = args[0] as NPC
-        if (getQuestStage(player, "Shield of Arrav") == 70 && getQuestStage(player, "The Golem") == 3 && getQuestStage(player, "The Dig Site") == 3) {
-            options("Talk about quest. (Shield of Arrav)", "Talk about quest. (The Golem)", "Talk about quest. (The Dig Site)", "Nevermind.")
-            stage = 0
-        } else if (getQuestStage(player, "Shield of Arrav") == 70) {
-            end()
-            openDialogue(player, CuratorHaigHalenSOADialogue())
-        } else if (getQuestStage(player, "The Golem") >= 3) {
-            end()
-            openDialogue(player, CuratorHaigHalenGolemDialogue())
-        } else if (getQuestStage(player, "The Dig Site") > 1) {
-            end()
-            openDialogue(player, CuratorHaigHelenDialogueFile())
-        } else {
-            npc(FacialExpression.HAPPY, "Welcome to the museum of Varrock.")
-            stage = END_DIALOGUE
-        }
-        return true
-    }
 
     override fun handle(interfaceId: Int, buttonId: Int): Boolean {
         when (stage) {
