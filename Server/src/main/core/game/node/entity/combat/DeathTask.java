3e420b46fb78f94fd16148e4403b97a89673519a
CC <szumaster41@gmail.com>
2024-08-20 02:05:17 +0200

    Docs

--- Server/src/main/core/game/node/entity/combat/DeathTask.java
+++ Server/src/main/core/game/node/entity/combat/DeathTask.java
@@ -76,8 +76,7 @@ public final class DeathTask extends NodeTask {
         e.getProperties().setTeleportLocation(spawn);
         e.unlock();
         e.finalizeDeath(killer);
-        e.getImpactHandler().getImpactLog().clear();// check if this needs to be
-        // before finalize
+        e.getImpactHandler().getPlayerImpactLog().clear();
         e.getImpactHandler().setDisabledTicks(4);
         e.dispatch(new SelfDeathEvent(killer));
     }
