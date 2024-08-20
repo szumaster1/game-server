801db2c405b204f78228e3b6506b309074e1fa0f
CC <szumaster41@gmail.com>
2024-08-20 02:12:06 +0200

    Rework the Tutorial Guide NPC

--- Server/src/main/content/region/misc/handlers/tutorial/TutorialGuideNPC.kt
+++ Server/src/main/content/region/misc/handlers/tutorial/TutorialGuideNPC.kt
@@ -1,13 +1,9 @@
 package content.region.misc.handlers.tutorial
 
-import core.api.animate
-import core.api.consts.Animations
 import core.api.consts.NPCs
 import core.api.sendChat
 import core.game.node.entity.npc.NPC
 import core.game.node.entity.npc.NPCBehavior
-import core.game.world.GameWorld
-import core.game.world.update.flag.context.Animation
 import core.tools.RandomFunction
 
 /**
@@ -17,9 +13,7 @@ class TutorialGuideNPC : NPCBehavior(NPCs.TUTORIAL_GUIDE_8591) {
 
     override fun tick(self: NPC): Boolean {
         if (RandomFunction.random(100) < 15) {
-            sendChat(self, "Welcome to ${GameWorld.settings!!.name}!")
-            animate(self, Animation(Animations.HUMAN_WAVE_863))
-            sendChat(self, "You can skip the tutorial by talking to me!", 2)
+            sendChat(self, "You can skip the tutorial by talking to me!")
         }
         return true
     }
