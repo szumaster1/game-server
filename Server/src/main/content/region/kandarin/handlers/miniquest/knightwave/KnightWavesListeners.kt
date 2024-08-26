5893b5e5604652199687d17fddf3cd82c615b1b1
szumaster <166091306+szumaster3@users.noreply.github.com>
2024-08-27 01:32:45 +0200

    Cleaned comments

--- Server/src/main/content/region/kandarin/handlers/miniquest/knightwave/KnightWavesListeners.kt
+++ Server/src/main/content/region/kandarin/handlers/miniquest/knightwave/KnightWavesListeners.kt
@@ -9,21 +9,8 @@ import core.game.world.GameWorld
 import core.game.world.map.Location
 import core.tools.RED
 
-/*
-| Wave	| Name			| Level | Weak against	| Stat lowered		|
-|-------|---------------|-------|---------------|-------------------|
-| 1		| Sir Bedivere	|  110  | Slash attacks	| Strength			|
-| 2		| Pelleas	    |  112  | Slash attacks	| Defence			|
-| 3		| Sir Tristram	|  115  | Stab attacks	| Strength			|
-| 4		| Sir Palomedes	|  118  | Crush attacks	| Attack			|
-| 5		| Lucan			|  120  | Slash attacks	| Defence			|
-| 6		| Gawain		|  122  | Stab attacks	| Attack			|
-| 7		| Sir Key		|  124  | Crush attacks	| Defence			|
-| 8		| Sir Lancelot	|  127  | Slash attacks	| Defence, Strength	|
- */
 /**
  * Represents The Knight Waves Training Grounds.
- * Mini-quest located on the top floor of Camelot castle.
  */
 class KnightWavesListeners : InteractionListener {
 
