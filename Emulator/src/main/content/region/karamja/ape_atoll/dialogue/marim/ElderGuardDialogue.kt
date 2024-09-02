7ed4c1c0dc8d18fee882c2d2cfab3e9c77b4ef7d
szumaster <166091306+szumaster3@users.noreply.github.com>
2024-09-02 03:18:01 +0200

    Relocation of classes that are in wrong packages

---
+++ Emulator/src/main/content/region/karamja/ape_atoll/dialogue/marim/ElderGuardDialogue.kt
@@ -0,0 +1,64 @@
+package content.region.karamja.ape_atoll.dialogue.marim
+
+import cfg.consts.NPCs
+import core.api.toIntArray
+import core.game.dialogue.Dialogue
+import core.game.dialogue.FacialExpression
+import core.game.node.entity.npc.NPC
+import core.game.node.entity.player.Player
+import core.plugin.Initializable
+
+/**
+ * Represents the Elder Guard dialogue.
+ */
+@Initializable
+class ElderGuardDialogue(player: Player? = null) : Dialogue(player){
+
+    val ids = 4025..4031
+
+    override fun open(vararg args: Any?): Boolean {
+        npc = args[0] as NPC
+
+        //todo check player pos, if outside, player needs to be unable from entering without speaking, but currently player can clip through monke
+        var outside = true
+        //todo check if monke is in the corner
+        var cornerGuard = false
+
+        if (player.equipment.containsAtLeastOneItem(ids.toIntArray())) {
+            if (outside) {
+                npc(FacialExpression.OLD_ANGRY1, "Grrr ... What do you want?").also { stage = 10 }
+            } else if(cornerGuard){
+                npc(FacialExpression.OLD_ANGRY1, "Grrr ... What do you want?").also { stage = 20 }
+            } else {
+                npc(FacialExpression.OLD_ANGRY1, "Move!").also { stage = 99 }
+            }
+        } else {
+            //todo monke is gonna knock you out if you don't have a monkey gree gree thang, chicken wang
+        }
+
+        return true
+    }
+
+    override fun handle(interfaceId: Int, buttonId: Int): Boolean {
+        when(stage){
+            10 -> playerl(FacialExpression.ASKING, "I must speak with Awowogei on a subject of great import.").also { stage ++ }
+            11 -> npc(FacialExpression.OLD_NORMAL, "Only the Captain of the Monkey Guard or those he authorizes may enter this building. You will need his permission to enter.").also { stage++ }
+            12 -> player(FacialExpression.ASKING, "Who is the Captain of the Monkey Guard?").also { stage ++ }
+            13 -> player(FacialExpression.ASKING, "He goes by the name of Kruk").also { stage = 99 }
+
+            20 -> player(FacialExpression.ASKING, "I would like to leave now.").also { stage ++ }
+            21 -> npc(FacialExpression.OLD_NORMAL, "As you wish.").also { stage = 99 }
+
+            99 -> end()
+        }
+        return true
+    }
+
+    override fun newInstance(player: Player?): Dialogue {
+        return ElderGuardDialogue(player)
+    }
+
+    override fun getIds(): IntArray {
+        return intArrayOf(NPCs.ELDER_GUARD_1461, NPCs.ELDER_GUARD_1462)
+    }
+}
---
+++ Emulator/src/main/content/region/karamja/ape_atoll/quest/mm/dialogue/GarkorFirstDialogue.kt
@@ -0,0 +1,35 @@
+package content.region.karamja.ape_atoll.quest.mm.dialogue
+
+import core.api.setQuestStage
+import core.game.dialogue.DialogueFile
+import core.game.dialogue.FacialExpression
+
+/**
+ * Represents the Garkor first dialogue.
+ */
+class GarkorFirstDialogue: DialogueFile() {
+
+    override fun handle(componentID: Int, buttonID: Int) {
+        when (stage) {
+            0 -> playerl(FacialExpression.NEUTRAL, "Hello?").also { stage++ }
+            1 -> npcl(FacialExpression.NEUTRAL, "A fine day you have chosen to visit this hellish island, human.").also { stage++ }
+            2 -> playerl(FacialExpression.NEUTRAL, "Good day to you too Sergeant. I've been sent by your King Narnode to -").also { stage++ }
+            3 -> npcl(FacialExpression.NEUTRAL, "Investigate the circumstances surrounding the mysterious disappearance of my squad. Yes, I know...").also { stage++ }
+            4 -> playerl(FacialExpression.NEUTRAL, "How did you know that?").also { stage++}
+            5 -> npcl(FacialExpression.NEUTRAL, "The King and I are still in communication, albeit sporadic. I decided I need a human of your calibre to assist me. It is pleasing to see you are still alive.").also { stage++ }
+            6 -> playerl(FacialExpression.NEUTRAL, "Why do you need a human?").also { stage++}
+            7 -> npcl(FacialExpression.NEUTRAL, "There is more going on than meets your eye, human. Did you not find it strange that an entire squad be sent to decomission a shipyard?").also { stage++ }
+            8 -> playerl(FacialExpression.NEUTRAL, "Well -").also { stage++}
+            9 -> npcl(FacialExpression.NEUTRAL, "Indeed. But there are more pressing matters at hand. Three of my squad have been captured and placed in the jail. They are watched over by somewhat overpowering guards.").also { stage++ }
+            10 -> npcl(FacialExpression.NEUTRAL, "Before we can resume our original mission we must rescue them.").also { stage++ }
+            11 -> playerl(FacialExpression.NEUTRAL, "I know about the guards - I had to sneak out between the change of shifts.").also { stage++}
+            12 -> npcl(FacialExpression.NEUTRAL, "Trust me; we too have considered this, but whilst it is possible for one, it is near impossible for three.").also { stage++ }
+            13 -> npcl(FacialExpression.NEUTRAL, "We have considered many things. I have my squad mage and sappers working below us right now. My assassin Karam, is operating in the village itself.").also { stage++ }
+            14 -> npcl(FacialExpression.NEUTRAL, "I remain here so that I may overhear Awowogei's plans.").also { stage++ }
+            15 -> {
+                end()
+                setQuestStage(player!!, "Monkey Madness", 30)
+            }
+        }
+    }
+}
