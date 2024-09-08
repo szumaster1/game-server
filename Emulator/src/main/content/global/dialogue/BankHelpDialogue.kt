87c1f7a4d99508be0e70fb77b8cf326e64890c94
szumaster <szumaster2009@gmail.com>
2024-09-09 00:58:14 +0200

    Corrected title of bank assistance interface

--- Emulator/src/main/content/global/dialogue/BankHelpDialogue.kt
+++ Emulator/src/main/content/global/dialogue/BankHelpDialogue.kt
@@ -2,13 +2,13 @@ package content.global.dialogue
 
 import cfg.consts.Components
 import cfg.consts.Items
+import core.Configuration
 import core.api.openInterface
 import core.api.sendDialogue
 import core.api.sendItemDialogue
 import core.api.setInterfaceText
 import core.game.dialogue.DialogueFile
 import core.game.node.item.Item
-import core.game.world.GameWorld
 import core.tools.START_DIALOGUE
 
 /**
@@ -20,39 +20,41 @@ class BankHelpDialogue : DialogueFile() {
         when (stage) {
             START_DIALOGUE -> options("Check Bank Value", "Banking Assistance", "Close").also { stage++ }
             1 -> when (buttonID) {
-                1 -> player?.let {
+                1 -> player?.let { it ->
                     end()
                     val wealth = it.bank.wealth
                     if (wealth > 0) {
                         val word = if (wealth != 1) "coins" else "coin"
-                        sendItemDialogue(
-                            it,  if (wealth < 2)     Item(Items.COINS_8890, wealth)
-                            else if (wealth < 3)     Item(Items.COINS_8891, wealth)
-                            else if (wealth < 4)     Item(Items.COINS_8892, wealth)
-                            else if (wealth < 5)     Item(Items.COINS_8893, wealth)
-                            else if (wealth < 25)    Item(Items.COINS_8894, wealth)
-                            else if (wealth < 100)   Item(Items.COINS_8895, wealth)
-                            else if (wealth < 250)   Item(Items.COINS_8896, wealth)
-                            else if (wealth < 1000)  Item(Items.COINS_8897, wealth)
-                            else if (wealth < 10000) Item(Items.COINS_8898, wealth)
-                            else Item(Items.COINS_8899, wealth), "Your bank is worth <col=a52929>${wealth}</col> ${word}.")
+                        val itemMap = mapOf(
+                            2       to Items.COINS_8890,
+                            3       to Items.COINS_8891,
+                            4       to Items.COINS_8892,
+                            5       to Items.COINS_8893,
+                            25      to Items.COINS_8894,
+                            100     to Items.COINS_8895,
+                            250     to Items.COINS_8896,
+                            1000    to Items.COINS_8897,
+                            10000   to Items.COINS_8898,
+                            Int.MAX_VALUE to Items.COINS_8899
+                        )
+                        return sendItemDialogue(it,itemMap.entries.first { wealth < it.key }.let { Item(it.value, wealth) }, "Your bank is worth <col=a52929>${wealth}</col> ${word}.")
                     } else {
                         sendDialogue(it, "You have no valuables in your bank.")
                     }
                 }
+
                 2 -> player?.let {
                     end()
                     it.bank.close()
                     openInterface(it, Components.BANK_V2_HELP_767).also {
-                        if(player!!.interfaceManager.isOpened)
-                        setInterfaceText(
-                            player!!,
-                            "Bank of WHAT THE FUCK Help",
-                            Components.BANK_V2_HELP_767,
-                            40
-                        )
+                        if (player!!.interfaceManager.isOpened)
+                            setInterfaceText(player!!,
+                                "Bank of ${Configuration.SERVER_NAME} - Help",
+                                Components.BANK_V2_HELP_767, 40
+                            )
                     }
                 }
+
                 3 -> end()
             }
         }
