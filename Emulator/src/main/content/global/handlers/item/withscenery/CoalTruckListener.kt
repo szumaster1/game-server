97397279cb439a0eae6de84dfcce30420c5f584c
szumaster <szumaster2009@gmail.com>
2024-09-18 02:53:14 +0200

    Improved the CoalTruckListener

--- Emulator/src/main/content/global/handlers/item/withscenery/CoalTruckListener.kt
+++ Emulator/src/main/content/global/handlers/item/withscenery/CoalTruckListener.kt
@@ -5,15 +5,64 @@ import org.rs.consts.Items
 import org.rs.consts.Scenery
 import core.game.interaction.IntType
 import core.game.interaction.InteractionListener
+import core.game.node.entity.player.Player
 import core.game.node.entity.player.link.diary.DiaryType
 import core.game.node.item.Item
 
 /**
- * Coal truck listener.
+ * Represents the coal truck option interaction listener.
  */
 class CoalTruckListener : InteractionListener {
 
+    private fun calculateAvailableSpace(player: Player, coalInTruck: Int): Int {
+        return if (inEquipment(player, Items.SEERS_HEADBAND_1_14631)) {
+            when {
+                isDiaryComplete(player, DiaryType.SEERS_VILLAGE, 2) -> 196 - coalInTruck
+                isDiaryComplete(player, DiaryType.SEERS_VILLAGE, 1) -> 168 - coalInTruck
+                isDiaryComplete(player, DiaryType.SEERS_VILLAGE, 0) -> 140 - coalInTruck
+                else -> 120 - coalInTruck
+            }
+        } else {
+            120 - coalInTruck
+        }
+    }
+
+    private fun calculateMaxCoalCapacity(player: Player): Int {
+        return when {
+            inEquipment(player, Items.SEERS_HEADBAND_1_14631) && isDiaryComplete(player, DiaryType.SEERS_VILLAGE, 2) -> 196
+            inEquipment(player, Items.SEERS_HEADBAND_1_14631) && isDiaryComplete(player, DiaryType.SEERS_VILLAGE, 1) -> 168
+            inEquipment(player, Items.SEERS_HEADBAND_1_14631) && isDiaryComplete(player, DiaryType.SEERS_VILLAGE, 0) -> 140
+            else -> 120
+        }
+    }
+
     override fun defineListeners() {
+
+        /*
+         * Add coal to truck.
+         */
+
+        onUseWith(IntType.SCENERY, Items.COAL_453, Scenery.COAL_TRUCK_2114) { player, _, _ ->
+            val coalInTruck = getAttribute(player, ATTRIBUTE_COAL_TRUCK_INVENTORY, 0)
+            var coalInInventory = amountInInventory(player, Items.COAL_453)
+
+            val maxCoalCapacity = calculateMaxCoalCapacity(player)
+
+            if (coalInInventory + coalInTruck >= maxCoalCapacity) {
+                coalInInventory = maxCoalCapacity - coalInTruck
+                sendMessage(player, message)
+            }
+
+            if (removeItem(player, Item(Items.COAL_453, coalInInventory))) {
+                setAttribute(player, "/save:$ATTRIBUTE_COAL_TRUCK_INVENTORY", coalInTruck + coalInInventory)
+            }
+            return@onUseWith true
+        }
+
+        /*
+         * Withdraw coal from truck.
+         */
+
         on(Scenery.COAL_TRUCK_2114, IntType.SCENERY, "remove-coal") { player, _ ->
             var coalInTruck = getAttribute(player, ATTRIBUTE_COAL_TRUCK_INVENTORY, 0)
             if (coalInTruck == 0) {
@@ -31,46 +80,17 @@ class CoalTruckListener : InteractionListener {
             return@on true
         }
 
+        /*
+         * Check how many is stored into truck.
+         */
+
         on(Scenery.COAL_TRUCK_2114, IntType.SCENERY, "investigate") { player, _ ->
             val coalInTruck = getAttribute(player, ATTRIBUTE_COAL_TRUCK_INVENTORY, 0)
-            sendDialogue(
-                player, "There is currently $coalInTruck coal in the truck. " + "The truck has space for " +
-
-                        if(inEquipment(player, Items.SEERS_HEADBAND_1_14631)) {
-                            when {
-                                isDiaryComplete(player, DiaryType.SEERS_VILLAGE, 2) -> 196 - coalInTruck
-                                isDiaryComplete(player, DiaryType.SEERS_VILLAGE, 1) -> 168 - coalInTruck
-                                isDiaryComplete(player, DiaryType.SEERS_VILLAGE, 0) -> 140 - coalInTruck
-                                else -> 120 - coalInTruck
-                            }
-                        } else {
-                            120 - coalInTruck
-                        }
-                        + " more coal."
-            )
+            val availableSpace = calculateAvailableSpace(player, coalInTruck)
+            sendDialogue(player, "There is currently $coalInTruck coal in the truck. The truck has space for $availableSpace more coal.")
             return@on true
         }
 
-        onUseWith(IntType.SCENERY, Items.COAL_453, Scenery.COAL_TRUCK_2114) { player, _, _ ->
-            val coalInTruck = getAttribute(player, ATTRIBUTE_COAL_TRUCK_INVENTORY, 0)
-            var coalInInventory = amountInInventory(player, Items.COAL_453)
-            val maxCoalCapacity = when {
-                inEquipment(player, Items.SEERS_HEADBAND_1_14631) && isDiaryComplete(player, DiaryType.SEERS_VILLAGE, 2) -> 196
-                inEquipment(player, Items.SEERS_HEADBAND_1_14631) && isDiaryComplete(player, DiaryType.SEERS_VILLAGE, 1) -> 168
-                inEquipment(player, Items.SEERS_HEADBAND_1_14631) && isDiaryComplete(player, DiaryType.SEERS_VILLAGE, 0) -> 140
-                else -> 120
-            }
-
-            if (coalInInventory + coalInTruck >= maxCoalCapacity) {
-                coalInInventory = maxCoalCapacity - coalInTruck
-                sendMessage(player, message)
-            }
-
-            if (removeItem(player, Item(Items.COAL_453, coalInInventory))) {
-                setAttribute(player, "/save:$ATTRIBUTE_COAL_TRUCK_INVENTORY", coalInTruck + coalInInventory)
-            }
-            return@onUseWith true
-        }
     }
 
     companion object {
