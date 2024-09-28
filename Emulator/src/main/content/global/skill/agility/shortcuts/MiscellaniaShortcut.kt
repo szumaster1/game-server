ee2f9d8eeb4b06c35542a1beb356f4027565a1d8
szumaster <182374694+szumaster1@users.noreply.github.com>
2024-09-28 16:36:07 +0200

    Removed miscellania shortcut access from right side

--- Emulator/src/main/content/global/skill/agility/shortcuts/MiscellaniaShortcut.kt
+++ Emulator/src/main/content/global/skill/agility/shortcuts/MiscellaniaShortcut.kt
@@ -28,88 +28,42 @@ class MiscellaniaShortcut : InteractionListener {
                 return@on true
             }
 
-            if (inBorders(player, 2571, 3854, 2575, 3870)) {
-                queueScript(player, 1, QueueStrength.NORMAL) { stage: Int ->
-                    when (stage) {
-                        0 -> {
-                            AgilityHandler.forceWalk(
-                                player,
-                                -1,
-                                Location(2572, 3862, 0),
-                                Location(2573, 3862, 0),
-                                Animation(Animations.JUMP_BRIDGE_769),
-                                animationCycles(Animations.JUMP_BRIDGE_769),
-                                0.0,
-                                null
-                            )
-                            return@queueScript keepRunning(player)
-                        }
-
-                        1 -> {
-                            AgilityHandler.forceWalk(
-                                player,
-                                -1,
-                                Location(2573, 3862, 0),
-                                Location(2576, 3862, 0),
-                                Animation(Animations.JUMP_OVER_OBSTACLE_6132),
-                                animationCycles(Animations.JUMP_OVER_OBSTACLE_6132),
-                                0.0,
-                                null
-                            )
-                            return@queueScript stopExecuting(player)
-                        }
-
-                        else -> return@queueScript stopExecuting(player)
+            queueScript(player, 1, QueueStrength.NORMAL) { stage: Int ->
+                when (stage) {
+                    0 -> {
+                        AgilityHandler.forceWalk(
+                            player,
+                            -1,
+                            Location(2572, 3862, 0),
+                            Location(2573, 3862, 0),
+                            Animation(Animations.JUMP_BRIDGE_769),
+                            animationCycles(Animations.JUMP_BRIDGE_769),
+                            0.0,
+                            null
+                        )
+                        return@queueScript delayScript(player, animationCycles(Animations.JUMP_BRIDGE_769))
                     }
-                }
-            } else {
-                queueScript(player, 1, QueueStrength.NORMAL) { stage: Int ->
-                    when (stage) {
-                        0 -> {
-                            AgilityHandler.forceWalk(
-                                player,
-                                -1,
-                                Location(2576, 3862, 0),
-                                Location(2573, 3862, 0),
-                                Animation(Animations.JUMP_OVER_OBSTACLE_6132),
-                                animationCycles(Animations.JUMP_OVER_OBSTACLE_6132),
-                                0.0,
-                                null
-                            )
-                            return@queueScript keepRunning(player)
-                        }
 
-                        1 -> {
-                            AgilityHandler.forceWalk(
-                                player,
-                                -1,
-                                location(2573, 3862, 0),
-                                location(2572, 3862, 0),
-                                Animation(Animations.JUMP_BRIDGE_769),
-                                animationCycles(Animations.JUMP_BRIDGE_769),
-                                0.0,
-                                null
-                            )
-                            return@queueScript stopExecuting(player)
-                        }
-
-                        else -> return@queueScript stopExecuting(player)
+                    1 -> {
+                        AgilityHandler.forceWalk(
+                            player,
+                            -1,
+                            Location(2573, 3862, 0),
+                            Location(2576, 3862, 0),
+                            Animation(Animations.JUMP_OVER_OBSTACLE_6132),
+                            animationCycles(Animations.JUMP_OVER_OBSTACLE_6132),
+                            0.0,
+                            null
+                        )
+                        return@queueScript stopExecuting(player)
                     }
+
+                    else -> return@queueScript stopExecuting(player)
                 }
             }
             return@on true
         }
-    }
-
-
-    override fun defineDestinationOverrides() {
-        setDest(IntType.SCENERY, intArrayOf(Scenery.BROKEN_PIER_41531), "step") { player, _ ->
-            return@setDest if (inBorders(player, 2576, 3854, 2587, 3870)) {
-                Location.create(2576, 3862, 0)
-            } else {
-                Location.create(2572, 3862, 0)
-            }
-        }
 
     }
+
 }
